d5ce476e6e8402a12bba7005a90f81630170105a
cristianromano <63170214+cristianromano@users.noreply.github.com>
2023-10-07 00:59:14 -0300

    update

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Motion, OrientationListenerEvent } from '@capacitor/motion';
+import { async } from 'rxjs';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { CapacitorFlash } from '@capgo/capacitor-flash';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,83 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  toggleValue: boolean = false;
+  accelHandler?: PluginListenerHandle;
+  orientacion?: OrientationListenerEvent;
+  x?: any;
+  y?: any;
+  z?: any;
+  audioD?: HTMLAudioElement;
+  audioI?: HTMLAudioElement;
+  audioV?: HTMLAudioElement;
+  audioH?: HTMLAudioElement;
 
-  constructor() {}
+  constructor() {
+    this.audioD = new Audio();
+    this.audioI = new Audio();
+    this.audioV = new Audio();
+    this.audioH = new Audio();
+  }
 
+  ngOnInit() {}
+
+  async start() {
+    this.accelHandler = await Motion.addListener(
+      'orientation',
+      async (event) => {
+        this.x = event.beta;
+        this.y = event.gamma;
+        this.z = event.alpha;
+
+        //Al cambiar la posición, a izquierda o a derecha, emitirá un sonido distinto para cada lado.
+        if (this.y > 50 && this.x >= -1 && this.x <= 50) {
+          if (this.audioD) {
+            this.audioD.src = '../../assets/derecho.mp3';
+            await this.audioD?.play();
+          }
+        }
+        //izquierda
+        if (this.y <= -50 && this.x >= -1 && this.x <= 50) {
+          if (this.audioI) {
+            this.audioI.src = '../../assets/izquierdo.mp3';
+            await this.audioI?.play();
+          }
+        }
+        //Al ponerlo horizontal, vibrará (por 5 segundos) y al mismo tiempo emitirá otro sonido.
+        if (this.x <= -1) {
+          await Haptics.vibrate();
+
+          if (this.audioH) {
+            this.audioH.src = '../../assets/horizontal.mp3';
+            await this.audioH?.play();
+          }
+        }
+        //Al ponerlo vertical, se encenderá la luz (por 5 segundos) y al mismo tiempo emitirá un sonido.
+        if (this.x >= 50) {
+          if (this.audioV) {
+            CapacitorFlash.switchOn({ intensity: 5 });
+            CapacitorFlash.toggle;
+            this.audioV.src = '../../assets/vertical.mp3';
+            await this.audioV?.play();
+          }
+        }
+
+        if (this.x < 50) {
+          if (this.audioV) {
+            CapacitorFlash.switchOff();
+          }
+        }
+      }
+    );
+  }
+
+  activar() {
+    if (this.toggleValue == true) {
+      this.start();
+    } else {
+      if (this.accelHandler) {
+        this.accelHandler.remove;
+      }
+    }
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'alarma-robo-1acc9',
+    appId: '1:89579225369:web:b8b5da71ab38e050f64588',
+    storageBucket: 'alarma-robo-1acc9.appspot.com',
+    apiKey: 'AIzaSyDp5EjMqdVRp-q_lbojeMdl3zJygIDJnTE',
+    authDomain: 'alarma-robo-1acc9.firebaseapp.com',
+    messagingSenderId: '89579225369',
+  },
   production: true
 };
